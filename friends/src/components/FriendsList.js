0c5699b345ee9ff742f87154204ca457891cf072
RachelKolk <rach.m.obrien@gmail.com>
2019-01-23 17:07:55 -0500

    friends list displaying to the screen

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -12,11 +12,15 @@ class FriendsList extends React.Component {
     componentDidMount() {
         axios.get("http://localhost:5000/friends")
         .then(res => {
-            console.log(res);
+            this.setState({
+                friends: res.data,
+                error: ''            
+            });
         })
         .catch(err => {
-            console.log(err);
+            this.setState({error: err.response.data});          
         });
+               
     }
 
     render() {
@@ -24,6 +28,13 @@ class FriendsList extends React.Component {
             <div className="FriendsList">
                 <h1>My Friends</h1>
 
+                {this.state.error && <h4>{this.state.error}</h4>}
+
+                {this.state.friends.map(friends => (
+                    <h2>{friends.name}</h2>
+                    // <h4>Age: {friends.age}</h4>
+                    // <h3>Email: {friends.email}</h3>
+                ))}
 
             </div>
         )
